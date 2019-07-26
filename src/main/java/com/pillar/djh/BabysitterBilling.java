9fec256185451f28976758b2542fb44a7d3d317e
Dave Hutira <djhutira@impiricalsoftware.com>
2019-07-25 22:27:27 -0400

    Replaced overly-complicated if statements with array based method. Removed Unknown error. Added Debug switch.

--- src/main/java/com/pillar/djh/BabysitterBilling.java
+++ src/main/java/com/pillar/djh/BabysitterBilling.java
@@ -1,5 +1,7 @@
 package com.pillar.djh;
 
+import java.util.Arrays;
+
 /**
  * Babysitter Billing App. 
  *
@@ -8,13 +10,14 @@ public class BabysitterBilling
 {
 	
 	public final static int RANGE_ERROR = -1;
-	final static int START_STOP_ERROR = -2;
-	final static int UNKNOWN_ERROR = -3;
-	final static int FAMILY_ID_ERROR = -4;
+	public final static int START_STOP_ERROR = -2;
+	public final static int FAMILY_ID_ERROR = -3;
+	
+	private final static boolean DEBUG = false;
+	
+	final int familyRate[] = new int[12];
 	
 	public int totalPay(int startHour, int stopHour, char familyId) {
-		
-		int totalPay = 0;
     	
     	//Check that start/stop hours are in range: All integers 1-12 inclusive are valid, 
     	//except that the start hour cannot be 4. 
@@ -44,35 +47,58 @@ public class BabysitterBilling
     		return 0;
     	}
     	
-    	//Calculate pay for Family 'A' $15 per hour before 11pm, $20 per hour after. 
-    	if(familyId == 'A') {
-    		//Determine if both start and stop are before 11, 
-    		//start is before 11 and stop is after 11, 
-    		//or both start and stop are after 11. 
-    		if((startHour >= 5 && startHour < 11) && (stopHour > 5 && stopHour <= 11 )) {
-    			totalPay = (stopHour - startHour) * 15; 
-    		} else if ((startHour >= 5 && startHour < 11) && (stopHour == 12 || (stopHour >= 1 && stopHour <=4))) {
-    			//Add in hours before 11pm. 
-    			totalPay = (11 - startHour) * 15;
-    			//Add in hours after 11pm. 
-    			if (stopHour == 12) {
-    				totalPay = totalPay + 20;
-    			} else {
-    				totalPay = totalPay + 40 + (stopHour - 1) * 20;
-    			}
-    		} else if ((startHour == 12 || (startHour >= 1 && startHour <= 4 )) &&
-    				   (stopHour >= 1 && stopHour <= 4)) {
-    			if(startHour == 12) {
-    				totalPay = 20; 
-    			}
-    			totalPay = totalPay + (stopHour - startHour) * 20;
-    		}
-    		return totalPay;
-    	}
-    	
-    	
-    	//If a known condition was not detected, assume failure and return Unknown Error. 
-    	return UNKNOWN_ERROR; 
+    	//At this point we've validated input data. 
+    	//Calculate total pay based on start/stop hour and rate array. 
+    	initRate(familyId, familyRate);
+    	return calculatePay(startHour, stopHour, familyRate);
     	
     }
+	
+	//Initialize Family A Pay Rate array: $15 before 11pm, $20 afterwards. 
+	private void initRate(char FamilyId, int[] familyRate) {
+		Arrays.fill(familyRate, 0, 6, 15 );
+		Arrays.fill(familyRate, 6, 12, 20 );
+		
+		if (DEBUG) {
+			System.out.println("\n\n");
+			for(int i=0; i<12;i++) {
+				System.out.println("i = " + i + ", rate = " + familyRate[i]);
+			}	
+		}
+	}
+	
+	//Convert hour to array offset.
+	private int convertHourToOffset(int hour) {
+		int offset;
+		
+		if (hour >= 5 && hour <= 12) {
+			offset = hour - 5;
+		} else {
+			offset = hour + 7;
+		}
+		return offset;
+	}
+	
+	//Calculate pay based on rate and start/stop hour.
+	private int calculatePay(int startHour, int stopHour, int[] payRate) {
+		int pay = 0;
+		int startOffset = convertHourToOffset(startHour);
+		int stopOffset = convertHourToOffset(stopHour);
+		
+		if (DEBUG) {
+			System.out.println("\nStart Offset: " + startOffset);
+			System.out.println("Stop Offset: " + stopOffset);
+		}
+			
+		for (int i = startOffset; i < stopOffset; i++) {
+			pay += payRate[i];
+			
+			if (DEBUG) {
+				System.out.println("Pay Rate[" + i + "] = " + payRate[i]);
+			}
+			
+		}
+		return pay;
+	}
+	
 }
